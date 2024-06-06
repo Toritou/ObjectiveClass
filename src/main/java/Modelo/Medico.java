7c229a26a5ff2bf5d2f39a40ecd6f53611c6a0e3
Toro <l.toro05@ufromail.cl>
2024-06-06 14:00:51 -0400

    Clases principales

---
+++ src/main/java/Modelo/Medico.java
@@ -0,0 +1,42 @@
+package Modelo;
+
+import java.util.Objects;
+
+public class Medico {
+    private String nombre;
+    private String especialidad;
+
+    public Medico(String nombre, String especialidad) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Medico medico = (Medico) obj;
+        return nombre.equals(medico.nombre) && especialidad.equals(medico.especialidad);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, especialidad);
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "nombre='" + nombre + '\'' +
+                ", especialidad='" + especialidad + '\'' +
+                '}';
+    }
+}
