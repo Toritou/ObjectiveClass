4f15e33ac44056a9da1fb84ad2366eb0c1a70a5f
Toritou <TuCorreo@ejemplo.com>
2024-07-06 19:03:19 -0400

    Modificacion para el funcionamiento del api del correo, solo disponible para el administrador por el momento

---
+++ src/main/java/Vista/VentanaMP.java
@@ -0,0 +1,11 @@
+package Vista;
+
+import javax.swing.*;
+
+public class VentanaMP {
+    private JButton salirButton;
+    private JButton agendarHoraButton1;
+    private JButton modificarHoraButton1;
+    private JButton verInformacionPersonalButton;
+    private JButton eliminarHoraButton1;
+}
